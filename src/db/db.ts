ab86373a589829df77844de120bb6a897883345a
freightduck <xilverfang12@gmail.com>
2024-11-11 21:52:52 +0100

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,12 +2,12 @@ import { connect } from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD
+// const MONGO_USERNAME = process.env.MONGO_USERNAME
+// const MONGO_PASSWORD = process.env.MONGO_PASSWORD
 
 const connectDB = async () => {
     try {
-        await connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.a1uva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+        await connect(`mongodb+srv://farmbuddyai:gMMPKz02YAEwo4K6@cluster0.a1uva.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0`)
         console.log("Conected to mongoDB Cluster successfully...🚀🚀")
     } catch (error) {
         console.log(error)
